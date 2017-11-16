f1fc2bd768e519cd3e26af87b99bef4da59dd263
liuxue <liuxue@nutown.cn>
2017-11-16 15:54:00 +0800

    新增阿里云邮箱

--- Scripts/Mail/Mail.py
+++ Scripts/Mail/Mail.py
@@ -12,13 +12,26 @@ class Mail:
         report_path = Attachment().last_file()  # 邮件绝对路径
         report_name = Attachment().report_name()  # 附件名
 
-        sender = "abigaleliu@126.com"  # 发件人邮箱
-        smtpserver = "smtp.126.com"  # 发件服务器
-        password = "miao2333"  # 发件人SMTP密码（126）
+        receiver_list = ["liuxue@nutown.cn"]
+
+        # 126邮箱
+        # sender = "abigaleliu@126.com"  # 发件人邮箱
+        # smtpserver = "smtp.126.com"  # 发件服务器
+        # password = "miao2333"  # 发件人SMTP密码（126）
+
+        # 阿里云邮箱
+        sender = "liuxue@nutown.cn"  # 发件人阿里云邮箱
+        smtpserver = "smtp.mxhichina.com"  # 阿里云企业邮箱服务器
+        password = "******"  # 阿里云企业邮箱不设SMTP密码,输入邮箱密码
+
+        # QQ邮箱
+        # sender = "abigale_liu@qq.com"  # 发件人邮箱
+        # smtpserver = "smtp.qq.com"  # 发件服务器
         # password = "bknznoizcpnnddff"  # 发件人SMTP密码（qq）
+
         port = 25  # 发件端口:25为非SSL,465/587为SSL
 
-        receiver = ",".join(["abigale_liu@qq.com"])  # 收件人邮箱
+        receiver = ",".join(receiver_list)  # 收件人邮箱
 
         body = "<h1>各位:</h1><p>附件为本周测试报告,请尽快查收</p>"  # 邮件正文
 
@@ -29,7 +42,7 @@ class Mail:
 
         msg = MIMEMultipart()  # 带附件的邮件对象
         msg["subject"] = "Test Report"  # 邮件标题
-        msg["from"] = sender  # 发件人d
+        msg["from"] = sender  # 发件人
         msg["to"] = receiver  # 收件人
         text = MIMEText(body, "html")
         msg.attach(text)
@@ -44,7 +57,7 @@ class Mail:
             s = smtplib.SMTP(smtpserver, port)  # SMTP对应端口25;SMTP_SSL对应端口465/587
             s.login(sender, password)
             s.sendmail(sender, receiver, msg.as_string())  # 发送邮件
-            print("succeed")
+            print("Succeed")
             s.quit()
         except smtplib.SMTPRecipientsRefused as e:
             print('Recipient refused')
@@ -56,20 +69,21 @@ class Mail:
             print('Sender refused')
             print(e)
         except smtplib.SMTPException as e:
+            print("SMTP Exception")
             print(e)
 
-    def mail_timer(self):
-        """
-        每周五晚上18点发送邮件
-        :return:
-        """
-        today = TimeTemp().mail_time()["weekday"]
-        hour = TimeTemp().mail_time()["hour"]
-        if today == "Fri" and hour == "18":
-            self.mail()
+    # def mail_timer(self):
+    #     """
+    #     每周五晚上18点发送邮件
+    #     :return:
+    #     """
+    #     today = TimeTemp().mail_time()["weekday"]
+    #     hour = TimeTemp().mail_time()["hour"]
+    #     if today == "Fri" and hour == "18":
+    #         self.mail()
 
 
 if __name__ == '__main__':
     # Mail().mail()
-    Mail().mail_timer()
+    Mail().mail()
 
